ffa7ba771130a4dd3e1da0af4b9707506adafa2f
AlexGrand <namelost@protonmail.com>
2019-05-29 12:56:42 +0800

    argparse: combining Optional and Positional arguments

--- projects/ex4 Dealing with Command kine Arguments/argprs.py
+++ projects/ex4 Dealing with Command kine Arguments/argprs.py
@@ -1,13 +1,17 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="increase output verbosity",
-                    action="store_true")
 parser.add_argument('square', type=int,
                     help="display a square of a given number")
+parser.add_argument("-v", "--verbosity", help="increase output verbosity",
+                    action="count", default=0)
 args = parser.parse_args()
 answer = args.square**2
-if args.verbose:
+
+# bugfix: replace == with >=
+if args.verbosity >= 2:
     print("the square of {} equals {}".format(args.square, answer))
+elif args.verbosity >= 1:
+    print(f"{args.square}^2 == {answer}")
 else:
     print(answer)
