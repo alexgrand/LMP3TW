66cd43d6917c12088e565ed2b9d5db18c7421472
AlexGrand <namelost@protonmail.com>
2019-05-30 13:01:16 +0800

    added: cat -A

--- projects/ex5 cat/cat.py
+++ projects/ex5 cat/cat.py
@@ -10,6 +10,9 @@ parser.add_argument("cat", help=cat_help)
 parser.add_argument("-b", "--number-nonblank",
                     action="store_true",
                     help="number nonempty output lines, overrides -n")
+parser.add_argument("-A", "--show-all",
+                    help="equivalent to -vET",
+                    action="store_true")
 
 args = parser.parse_args()
 fl_name = args.cat
@@ -19,7 +22,7 @@ def file_exists(fl_name):
     if os.path.exists(fl_name):
         return True
     else:
-        print(f"\n\tcat: {fl_name}: No such file or directory!\n")
+        print(f"cat: {fl_name}: No such file or directory!")
         return None
 
 
@@ -45,20 +48,36 @@ def read_lines(fl_name):
 
 def cat_b(fl_name):
     content = read_lines(fl_name)
-    numberd_content = ''
+    nw_content = []
     line_num = 0
 
     for line in content:
         if not line == '\n':
             line_num += 1
-            numberd_content += f"\t{line_num}  {line}"
+            nw_content.append(f"\t{line_num}  {line}")
         else:
-            numberd_content += line
-    
-    return numberd_content
+            nw_content.append(line)
 
+    return nw_content
+
+
+def cat_A(fl_name):
+    content = read_lines(fl_name)
+    nw_content = []
+
+    for line in content:
+        nw_content.append(f"{line}$")
+
+    return nw_content
+
+
+def cat_print(content):
+    for line in content:
+        print(line)
 
 if args.number_nonblank:
-    print(cat_b(fl_name))
+    cat_print(cat_b(fl_name))
+elif args.show_all:
+    cat_print(cat_A(fl_name))
 else:
     print(read_file(fl_name))
