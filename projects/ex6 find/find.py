e8454f96b3a984b5dbcb8cf4a3897813fbf896e0
AlexGrand <namelost@protonmail.com>
2019-06-01 23:02:00 +0800

    added path_print and  fn for -type arg

--- projects/ex6 find/find.py
+++ projects/ex6 find/find.py
@@ -20,6 +20,7 @@ class Find(object):
         self.parse_args()
         self.sort_args()
         self.cmds_start()
+        self.path_print()
 
     def parse_args(self):
         self.parser.add_argument(
@@ -32,6 +33,10 @@ class Find(object):
                 Base of file name (the path with the leading directories
                 removed) matches shell pattern
             """,
+        )
+        self.parser.add_argument(
+            "-type",
+            help="file of type directory",
         )
 
         self.args = vars(self.parser.parse_args())
@@ -64,22 +69,40 @@ class Find(object):
         if no_commands:
             self.find()
 
+    def path_print(self):
+        for path in self.paths:
+            print(path)
+
     def find(self):
         self.path_exists()
-        print(self.path)
-        return self.path
+        self.paths.append(self.path)
+        return self.paths
 
     def name(self):
         self.path_exists()
-        self.paths = list(Path(self.path).glob(f"**/{self.args['name']}"))
+        self.paths = list(Path(self.path).rglob(self.args['name']))
 
         if not self.paths:
             print(f"find: unknown pattern `{self.args['name']}`")
             exit(0)
-        else:
-            for path in self.paths:
-                print(path)
 
+    def type(self):
+        type_of = self.args['type']
+        if type_of == 'd':
+            check = 'is_dir'
+        elif type_of == 'f':
+            check = 'is_file'
+        else:
+            print(f"find: Unknown argument to -type: {type_of}")
+            exit(0)
+
+        nw_paths = []
+        for path in self.paths:
+            checked_type = getattr(Path(path), check)()
+            if checked_type:
+                nw_paths.append(path)
+
+        self.paths = nw_paths
 
 finder = Find()
 finder.start()
