9dda385d4b989f2f15fb928a9fa42411523c708d
AlexGrand <namelost@protonmail.com>
2019-06-01 20:49:26 +0800

    added -name arg and fixed cmd_start fn

--- projects/ex6 find/find.py
+++ projects/ex6 find/find.py
@@ -8,8 +8,13 @@ class Find(object):
         self.parser = argparse.ArgumentParser()
         self.path_obj = Path()
         self.path = ''
+        self.paths = []
         self.cmds = []
         self.args = {}
+
+    def invalid(self):
+        print(f"find: ‘{self.path}’: No such file or directory")
+        exit(1)
 
     def start(self):
         self.parse_args()
@@ -20,6 +25,15 @@ class Find(object):
         self.parser.add_argument(
             "find",
             help="search for files in a directory hierarchy")
+        self.parser.add_argument(
+            "-n", "-name", "--name",
+            help="""
+            pattern
+                Base of file name (the path with the leading directories
+                removed) matches shell pattern
+            """,
+            action="store_true"
+        )
 
         self.args = vars(self.parser.parse_args())
         return self.args
@@ -35,20 +49,37 @@ class Find(object):
     def path_exists(self):
         self.path_obj = Path(self.path)
         if not self.path_obj.exists():
-            print(f"find: ‘{self.path}’: No such file or directory")
-            exit(1)
+            self.invalid()
         else:
             return True
 
     def cmds_start(self):
-        for cmd in self.cmds:
-            fn = getattr(self, cmd)
-            fn()
+        args = dict(self.args)
+        args.pop('find')
+        cmd_is_active = list(args.values()).__contains__(True)
+
+        if cmd_is_active:
+            for cmd in args:
+                if args[cmd]:
+                    fn = getattr(self, cmd)
+                    fn()
+        else:
+            self.find()
 
     def find(self):
         self.path_exists()
         print(self.path)
         return self.path
+
+    def name(self):
+        self.paths = list(Path('.').glob(self.path))
+
+        if not self.paths:
+            self.invalid()
+        else:
+            for path in self.paths:
+                print(f"./{path}")
+
 
 finder = Find()
 finder.start()
