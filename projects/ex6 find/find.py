8ec208ae293e27cdbea2843b5884464ee050fefa
AlexGrand <namelost@protonmail.com>
2019-06-01 14:41:46 +0800

    added Find class

--- projects/ex6 find/find.py
+++ projects/ex6 find/find.py
@@ -1,17 +1,44 @@
-import sys
-import os
 import argparse
 from pathlib import Path
+from sys import exit
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("find",
-                        help="search for files in a directory hierarchy")
+class Find(object):
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.path_obj = Path()
+        self.path = ''
+        self.cmds = []
+        self.args = {}
 
-    return vars(parser.parse_args())
+    def start(self):
+        self.parse_args()
+        self.sort_args()
+        self.check_path()
 
-u_path = parse_args().get('find')
+    def parse_args(self):
+        self.parser.add_argument(
+            "find",
+            help="search for files in a directory hierarchy")
 
-print(os.path.exists(u_path))
+        self.args = vars(self.parser.parse_args())
+        return self.args
 
+    def sort_args(self):
+        for cmd in self.args:
+            self.cmds.append(cmd)
+
+            if cmd == 'find':
+                self.path = self.args.get(cmd)
+        return self.cmds, self.path
+
+    def check_path(self):
+        self.path_obj = Path(self.path)
+        if not self.path_obj.exists():
+            print(f"find: ‘{self.path}’: No such file or directory")
+            exit(1)
+
+finder = Find()
+finder.start()
+print(finder.args)
+print(finder.cmds, finder.path)
