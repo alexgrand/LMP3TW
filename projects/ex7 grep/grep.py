505bc601070f56bef35bec0f17b1abc6909ab0fe
AlexGrand <namelost@protonmail.com>
2019-06-03 19:55:55 +0800

    added fn for checking activated cmds

--- projects/ex7 grep/grep.py
+++ projects/ex7 grep/grep.py
@@ -16,21 +16,28 @@ class Grep(object):
         self.parser = argparse.ArgumentParser()
         self.args = []
         self.path = ''
+        self.cmds = []
 
     def start(self):
         self.parse_args()
         self.check_path()
         self.get_strings()
+        self.has_active_cmds()
         self.parse_strings()
 
     def parse_args(self):
         self.parser.add_argument(
             "pattern",
-            help="searches for PATTERN in each FILE"
+            help="searches for PATTERN in FILE"
         )
         self.parser.add_argument(
             "file",
             help='provide file for search'
+        )
+        self.parser.add_argument(
+            "-i", "--ignore-case",
+            help="ignore case distinctions",
+            action="store_true"
         )
 
         self.args = vars(self.parser.parse_args())
@@ -56,6 +63,17 @@ class Grep(object):
         fl.close()
 
         return self.strings
+
+    def has_active_cmds(self):
+        args = dict(self.args)
+        args.pop('file')
+        args.pop('pattern')
+
+        values = list(args.values())
+        if not values.__contains__(True):
+            return
+        else:
+            self.cmds = [cmd for cmd in args.keys() if args[cmd]]
 
     def parse_strings(self):
         for string in self.strings:
